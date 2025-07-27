65b2b358e2a5ed779eac578a7cd3dea9a5a857bf
smartcode54 <samartcode@gmail.com>
2025-07-27 21:26:31 +0700

    add calculate_area

---
+++ calculateArea/calculate_Area.js
@@ -0,0 +1,12 @@
+// calculate area
+let lenght;
+let width;
+
+function calculateArea() {
+    lenght = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+
+let area = lenght * width;
+document.getElementById('result').innerText =`The area of the rectangle is: ${area}`;
+console.log('area is: ' + area);
+}
